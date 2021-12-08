0d5ac1becdd7e02bf0f5ea3259ce8279bd4855e4
hkdywg <hkdywg@163.com>
2021-12-08 23:05:42 +0800

    Update: kernel workspace config
    Signed-off-by: hkdywg <hkdywg@163.com>

--- scripts/kernel/linux.sh
+++ scripts/kernel/linux.sh
@@ -10,21 +10,20 @@ LINUX_KERNEL_GITHUB=${5%X}
 LINUX_KERNEL_SRC=${6%X}
 
 OUTPUT=${ROOT}/output/kernel
+WORKSPACE=${ROOT}/workspace
+
+[ ! -d ${WORKSPACE} ] && mkdir -p ${WORKSPACE}
+[ ! -d ${OUTPUT} ] && mkdir -p ${OUTPUT}
 
 case ${LINUX_KERNEL_SRC} in
     # Get from github
     1)
-        if [ ! -d ${ROOT}/dl/${LINUX_KERNEL_NAME} ];then
-            cd ${ROOT}/dl
-            git clone ${LINUX_KERNEL_GITHUB} --depth=1 --branch v${LINUX_KERNEL_VERSION}
-        else
-            git pull
-        fi
-        mkdir -p ${OUTPUT}/${LINUX_KERNEL_NAME}
-        cp ${ROOT}/dl/${LINUX_KERNEL_NAME} ${OUTPUT}/${LINUX_KERNEL_NAME} -rf
+        cd ${ROOT}/dl
+        git clone ${LINUX_KERNEL_GITHUB} --depth=1 --branch v${LINUX_KERNEL_VERSION}
+        cp ${ROOT}/dl/${LINUX_KERNEL_NAME} ${OUTPUT}/ -rf
         rm ${ROOT}/dl/${LINUX_KERNEL_NAME} -rf
         echo -e "\033[32m linux kernel download successed!! \033[0m"
-        
+        ln -s ${OUTPUT}/${LINUX_KERNEL_NAME} ${WORKSPACE}/kernel
         ;;
     2)
         echo -e "\033[31m this method is not support!!!\033[0m"
@@ -38,16 +37,16 @@ case ${LINUX_KERNEL_SRC} in
             cd ${ROOT}/dl
             wget ${LINUX_KERNEL_SITE}/${BASE_NAME}
         fi
-        mkdir -p ${OUTPUT}/${LINUX_KERNEL_NAME}
-        cp ${ROOT}/dl/${BASE_NAME} ${OUTPUT}/${LINUX_KERNEL_NAME}
-        cd ${OUTPUT}/${LINUX_KERNEL_NAME}
+        mkdir -p ${OUTPUT}/
+        cp ${ROOT}/dl/${BASE_NAME} ${OUTPUT}
+        cd ${OUTPUT}
         tar -xf ${BASE_NAME}
         if [ $? -ne 0 ];then
             echo -e "\033[31m Invalid tar operation \033[0m"
             exit -1
         fi
-        echo ${LINUX_KERNEL_VERSION} > ${OUTPUT}/${LINUX_KERNEL_NAME}/version
         echo -e "\033[32m linux kernel download successed!! \033[0m"
+        ln -s ${OUTPUT}/${BASE} ${WORKSPACE}/kernel
         ;;
 esac
 
