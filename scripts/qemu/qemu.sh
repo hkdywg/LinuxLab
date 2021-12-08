c415ed58102352af397b8ebf6c5443d060019a36
hkdywg <hkdywg@163.com>
2021-12-08 22:34:34 +0800

    Update: qemu config
    Signed-off-by: hkdywg <hkdywg@163.com>

--- scripts/qemu/qemu.sh
+++ scripts/qemu/qemu.sh
@@ -7,9 +7,7 @@ QEMU_WGET_SITE=${4%X}
 QEMU_GITHUB_SITE=${5%X}
 
 OUTPUT=${ROOT}/output
-
-
-echo ${ROOT} ${QEMU_SRC} $QEMU_VERSION
+WORKSPACE=${ROOT}/workspace
 
 [ ! -d ${OUTPUT} ] && mkdir -p ${OUTPUT}
 
@@ -27,16 +25,18 @@ if [ ${QEMU_SRC} = "1" ]; then
       echo "\033[31m qemu only support obove version \033[0m"
       exit -1
   fi
+  ln -s ${OUTPUT}/qemu ${WORKSPACE}/qemu
 fi
 
 if [ ${QEMU_SRC} = "2" ];then
-    QEMU_WGET_NAME=qemu-${QEMU_VERSION}.tar.bz2
+    QEMU_WGET_NAME=qemu-${QEMU_VERSION}.tar.xz
     if [ ! -f ${QEMU_WGET_NAME} ]; then
         cd ${ROOT}/dl/
         wget ${QEMU_WGET_SITE}/${QEMU_WGET_NAME}
     fi
     cp ${QEMU_WGET_NAME} ${OUTPUT}
     cd ${OUTPUT}
-    tar -jxf ${QEMU_WGET_NAME}
+    tar -xJf ${QEMU_WGET_NAME}
     rm ${QEMU_WGET_NAME}
+    ln -s ${OUTPUT}/qemu-${QEMU_VERSION} ${WORKSPACE}/qemu
 fi
