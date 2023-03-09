3c7180de796c2817e06a1542fb2091e4bdcd2823
hkdywg@163.com <hkdywg@163.com>
2023-03-09 17:40:23 +0800

    Add: qemu config
    Signed-off-by: hkdywg@163.com <hkdywg@163.com>

---
+++ scripts/qemu/qemu_config.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+# Establish qemu-system for arm64/arm32
+#
+# (C) 2023-03-09 hkdywg <hkdywg@163.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+
+ROOT=${1%X}
+QEMU_VERSION=${2%X}
+QEMU_SRC=${3%X}
+ARCH_CONFIG=${4%X}
+OUTPUT=${ROOT}/output/config/qemu
+WORKSPACE=${ROOT}/workspace
+QEMU_WORKSPACE=${WORKSPACE}/qemu
+MF=${WORKSPACE}/run_qemu.sh
+
+CPU_CORE=`cat /proc/cpuinfo | grep processor | wc -l`
+
+
+if [ ${ARCH_CONFIG} = "ARM64" ];then
+    # ARM 64-bit
+    EMULATER=aarch64-softmmu,aarch64-linux-user
+    CONFIG="--enable-kvm --enable-virtfs"
+elif [ ${ARCH_CONFIG} = "ARM32" ];then
+    # ARM 32-bit
+    EMULATER=arm-softmmu,arm-linux-user
+    CONFIG="--enable-virtfs --enable-kvm"
+else
+    echo -e "\033[31m Invalid arch config, not support yet! \033[0m"
+    exit -1
+fi
+   
+
+# config qemu and make makefile
+cd ${QEMU_WORKSPACE}
+./configure --target-list=${EMULATER} ${CONFIG}
+make -j${CPU_CORE}
+
+echo -e '#!/bin/bash' > ${MF}
+echo -e '#' >> ${MF}
+echo -e '# Qemu system debug' >> ${MF}
+echo -e '#' >> ${MF}
+echo -e 'ROOT=${OUTPUT}' >> ${MF}
+echo -e 'QEMU_DIR=' >> ${MF}
+echo -e '' >> ${MF}
+
+
